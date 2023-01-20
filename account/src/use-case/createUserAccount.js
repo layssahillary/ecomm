65c056b5afe86b6648abf326da8733725ebaa61b
Ferreira Layssa Hillary Santos <n917699@gruposantander.com>
2023-01-20 15:44:35 -0300

    criação da conta de usuario

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -1,4 +1,19 @@
-export const accounts = []; // criando array vazio para as accounts
+export const accounts = [
+  {
+    id: 1,
+    name: "usuario",
+    email: "usuario@email.com",
+    password: "54321",
+    createdDate: "2023-01-06",
+  },
+  {
+    id: 2,
+    name: "layssa",
+    email: "layssa@email.com",
+    password: "12334",
+    createdDate: "2023-01-06",
+  },
+]; // criando array vazio para as accounts
 
 export function createUserUseCase(name, email, password) { //função createuser
   
