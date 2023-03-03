fee1b7e26e80d01dfc31c547e8b9260f45f03c84
layssa <layssahillary@hotmail.com>
2023-03-03 16:59:14 -0300

    Ajustes PR - node express

--- product/src/config/dbConnect.js
+++ product/src/config/dbConnect.js
@@ -1,7 +1,4 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb://admin:secret@127.0.0.1:27017/ecomm-product?authSource=admin"); // conectando no mongo 
-
-let db = mongoose.connection; // variavel que guarda a conexão do mongo
-
+import mongoose from 'mongoose';
+mongoose.connect('mongodb://admin:secret@mongodb:27017/ecomm-product?authSource=admin');
+const db = mongoose.connection;
 export default db;
--- product/src/routes/index.js
+++ product/src/routes/index.js
@@ -1,15 +1,13 @@
-import express from "express";
 import categoriesRouter from "./categoriesRoutes.js";
 
 const routes = (app) => {
-    app.route('/').get((req,res) => {
-        res.status(200).send({titulo: "Ecomm-product"})
+    app.route('/').get((_req,res) => {
+        res.status(200).send('Ecomm Project')
     });  // aqui é a requisição default
 
     app.use(
-        express.json(),
         categoriesRouter
-    )
+    );
 }
 
  // centraliza as rotas
